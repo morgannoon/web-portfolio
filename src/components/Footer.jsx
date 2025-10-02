78290faea1f31d2401c9a9752ac8a2cb318f145c
Morgan Noonan <112260476+morgannoon@users.noreply.github.com>
2025-10-02 19:53:01 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,16 +11,16 @@ export default function Footer() {
             </div>
             <div className="case" style={{ flexDirection: "row" }}>
               <a href="https://www.linkedin.com/in/morganenoonan" target="_blank">
-                <img className="case img" src="../../../../public/images/linkedin.png" alt="LinkedIn" />
+                <img className="case img" src="/images/linkedin.png" alt="LinkedIn" />
               </a>
               <a href="https://github.com/morgannoon" target="_blank">
-                <img className="case img" src="../../../../public/images/gitcat.png" alt="git hub" />
+                <img className="case img" src="/images/gitcat.png" alt="git hub" />
               </a>
               <a href="https://www.instagram.com/ganmor_n" target="_blank">
-                <img className="case img" src="../../../../public/images/insta.png" alt="instagram" />
+                <img className="case img" src="/images/insta.png" alt="instagram" />
               </a>
               <a href="https://www.goodreads.com/user/show/187083363-morgan-noonan" target="_blank">
-                <img className="case img" src="../../../../public/images/goodreads.png" alt="good reads" />
+                <img className="case img" src="/images/goodreads.png" alt="good reads" />
               </a>
             </div>
           </div>
@@ -32,17 +32,17 @@ export default function Footer() {
             <div className="case contacts">
               <div className="contact-item">
                 <a href="tel:+19733036115">
-                  <img className="case img" src="../../../../public/images/phone.png" alt="phone" />
+                  <img className="case img" src="/images/phone.png" alt="phone" />
                   (973) 303-6115</a>
               </div>
               <div className="contact-item">
                 <a href="mailto:morgannoon2@gmail.com">
-                  <img className="case img" src="../../../../public/images/email.png" alt="email" />
+                  <img className="case img" src="/images/email.png" alt="email" />
                   morgannoon2@gmail.com</a>
               </div>
               <div className="contact-item">
                 <a href="mailto:men83@pitt.edu">
-                  <img className="case img" src="../../../../public/images/email.png" alt="email" />
+                  <img className="case img" src="/images/email.png" alt="email" />
                   men83@pitt.edu</a>
               </div>
             </div>
